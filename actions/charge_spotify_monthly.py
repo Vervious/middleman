a8622488e102a73cb539ad7064b5a79db0ec66bc
Benjamin Chan <me@vervious.com>
2017-09-09 21:11:06 -0700

    Update charge_spotify_monthly.py

--- actions/charge_spotify_monthly.py
+++ actions/charge_spotify_monthly.py
@@ -1,6 +1,15 @@
+import datetime
 
+# on the 10th day of every month, charge everyone $2.5
 def charge_spotify_monthly(state):
-    pass
-
-def stop_charge_spotify_monthly(state):
-    pass
+    month =  datetime.datetime.today().month
+    if ("spotify-last-paid-month" in state and state["spotify-last-paid-month"] == month):
+        return
+    if (datetime.datetime.today().day != 10):
+        return
+    state["ben-$"] = state.get("ben-$", 0) + 15
+    state["wei-$"] = state.get("weilian-$", 0) - 2.5
+    state["mic-$"] = state.get("weilian-$", 0) - 2.5
+    state["ray-$"] = state.get("weilian-$", 0) - 2.5
+    state["lucia-$"] = state.get("weilian-$", 0) - 2.5
+    state["spotify-last-paid-month"] = month
